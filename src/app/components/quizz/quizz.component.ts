286444dc715dfa8e6a2d0ced153da354081a924e
Rafael Lacerda <rlag@outlook.com>
2024-01-10 15:33:09 -0300

    Conclusão do projeto

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,64 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
+
+@Component({
+  selector: 'app-quizz',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent {
+  title:string = ''
+  questions:any 
+  questionSelected:any
+
+  ansewers:string[] = []
+  anserersSelected:string = ''
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  ngOnInit(){
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value:string){
+    this.ansewers.push(value)
+    this.nextStep()
+  }
+
+  async nextStep(){
+    this.questionIndex +=1
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.ansewers)
+      this.finished = true
+      this.anserersSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+    }
+  }
+
+  async checkResult(ansewers:string[]){
+    const result = ansewers.reduce((previous, current, i, arr) =>{
+      if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length ){
+        return previous
+      }else{
+        return current
+      }
+    } )
+    return result
+  }
+}
